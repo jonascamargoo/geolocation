47d92d52635bfa2738d361a5cf467a10b83deb7f
jonascamargoo <jonascamargo999@gmail.com>
2023-01-07 15:53:39 -0300

    comment

--- src/models/Author.ts
+++ src/models/Author.ts
@@ -1,5 +1,6 @@
 import mongoose, { Document, Schema } from "mongoose";
 
+// if i want to put the id field whithin IAuthor, i must call authorId insted of just id, it's because of Document class inherited
 export interface IAuthor {
     name: string;
 }
