44ff54b689c9c5b8f0160969835ed8956759de24
jonascamargoo <82424972+jonascamargoo@users.noreply.github.com>
2023-10-21 17:34:25 -0300

    Update Partner.ts

--- backend/src/models/Partner.ts
+++ backend/src/models/Partner.ts
@@ -17,6 +17,8 @@ export interface IPartner {
   }
 }
 
+//Quando eu adiciono o campo index: '2dshpere' no coverageArea.coordinates, da erro e a coleção simplesmente não aceita outro parceiro. Para resolver, preciso selecionar outro nome para a coleção em export default mongoose.model('Partner', PartnerSchema) para export default mongoose.model('Brewery', PartnerSchema) por exemplo, e deletar a coleção partners no mongo.
+
 const partnerSchema: Schema = new mongoose.Schema ({
   partnerId: {
     type: Number,
