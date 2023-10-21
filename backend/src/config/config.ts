b8e48c7738e430acfd10a6af078eb4816708ce7c
jonascamargoo <82424972+jonascamargoo@users.noreply.github.com>
2023-10-21 17:32:17 -0300

    Update config.ts

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -2,11 +2,6 @@ import dotenv from 'dotenv';
 
 dotenv.config()
 
-
-// senha mongo
-// iIR4rEkSNwmYbYay
-
-//para ele entender que a variável é um string e nao nula
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.ef2ybz3.mongodb.net/`;
